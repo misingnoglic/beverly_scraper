719610e844d0d40095616c70f9767da10f2be89e
arya <aboudaie@brandeis.edu>
2015-03-11 23:04:16 -0400

    Initial commit

---
+++ mrpaul.py
@@ -0,0 +1,29 @@
+import requests
+url = 'bhhs.bhusd.org/apps/staff/' #Url to parse from
+r = requests.get("http://" +url)
+
+from HTMLParser import HTMLParser
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
+    
+data = r.text
+data = data.splitlines()
+names = []
+for line in data:
+    if '''<span id="staff''' in line:
+        name = strip_tags(line)
+        name = name.strip('\t')
+        names.append(name)
+print names
